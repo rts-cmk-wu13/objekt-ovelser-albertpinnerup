434de936093416d5912a04a7823d83654e0855c5
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-02-03 10:31:04 +0100

    opgave 1 løst

---
+++ objekt.js
@@ -0,0 +1,19 @@
+let albert = {
+    navn: "Albert",
+    efternavn: "Pinnerup",
+    alder: 23,
+    by: "København NV",
+    hobbyer: ["Musik produktion", "Fotografi", "film"],
+    barn: {
+        navn: "Mio",
+        efternavn: "Banaszek Pinnerup"
+    }
+}
+
+console.log(albert.navn);
+console.log(albert["efternavn"]);
+console.log(albert.alder);
+console.log(albert["by"]);
+
+
+
